d42a96f929c8f5b4c01d732dc752d4cec29983b2
amitmjose <amit.jose@vonnue.com>
2023-05-15 17:34:38 +0530

    test change 2 - hash.bundle

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: { output: { filename: '[name].[hash].bundle.js' } },
 })
